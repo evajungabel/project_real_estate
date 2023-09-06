00019ee746d6205c8d2fc24beef8b917c9210315
evajungabel <eva.jungabel@gmail.com>
2023-09-06 18:02:55 +0200

    Tesztek

--- src/test/java/hu/progmasters/moovsmart/service/PropertyServiceTest.java
+++ src/test/java/hu/progmasters/moovsmart/service/PropertyServiceTest.java
@@ -8,9 +8,11 @@ import hu.progmasters.moovsmart.repository.PropertyRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
@@ -30,25 +32,36 @@ public class PropertyServiceTest {
     @Mock
     private PropertyRepository propertyRepository;
 
+    @Mock
+    private ModelMapper modelMapper;
+
     @InjectMocks
     private PropertyService propertyService;
 
     private Property property1;
 
+    private Property property2;
     private PropertyForm propertyForm1;
     private PropertyInfo propertyInfo1;
+    private PropertyInfo propertyInfo2;
     private CustomUser customUser1;
 
     private EstateAgent estateAgent1;
 
+
     @BeforeEach
     void init() {
+        ModelMapper modelMapper = new ModelMapper();
+
         property1 = new Property(1L, LocalDateTime.of(2022, Month.JANUARY, 1, 10, 10, 30), LocalDateTime.of(2023, Month.JANUARY, 1, 10, 10, 30), null, null, "Kuckó", PropertyType.HOUSE, 90, 5, 40000000, "Jó kis házikó",
                 "image/jpeg;base64,/2579j/4AAQSk", true, null, null, customUser1, estateAgent1);
-        customUser1 = new CustomUser(1L, "pisitke", "Kis Pistike", "pistike1234", "pistike@gmail.com", true, true, false, "123456789", List.of(CustomUserRole.ROLE_USER), List.of(property1), null);
+        property2 = new Property(2L, LocalDateTime.of(2021, Month.JANUARY, 1, 10, 10, 30), LocalDateTime.of(2022, Month.JANUARY, 1, 10, 10, 30), null, null, "Kuckó", PropertyType.HOUSE, 90, 6, 40000000, "Jó kis családi ház",
+                "image/jpeg;base64,/op79j/4AAQSk", true, null, null, customUser1, estateAgent1);
+        customUser1 = new CustomUser(1L, "pisitke", "Kis Pistike", "pistike1234", "pistike@gmail.com", true, true, false, LocalDateTime.of(2023, Month.JULY, 1, 10, 10, 30), "123456789", List.of(CustomUserRole.ROLE_USER), List.of(property1), null);
         estateAgent1 = new EstateAgent(1L, AgentRank.MEDIOR, "Hvj Bátran", 12, "hivjbatran@ingatlan.com", List.of(property1));
         propertyInfo1 = new PropertyInfo(1L, "Kuckó", 5, 40000000, "image/jpeg;base64,/2579j/4AAQSk");
-        propertyForm1 = new PropertyForm( "Kuckó", PropertyType.HOUSE, 90, 5, 40000000, "Jó kis házikó",
+        propertyInfo2 = new PropertyInfo(2L, "Kuckó", 6, 40000000, "image/jpeg;base64,/op79j/4AAQSk");
+        propertyForm1 = new PropertyForm("Kuckó", PropertyType.HOUSE, 90, 5, 40000000, "Jó kis házikó",
                 "image/jpeg;base64,/2579j/4AAQSk", "pistike");
     }
 
@@ -59,17 +72,33 @@ public class PropertyServiceTest {
     }
 
     @Test
-    void testSave_singleProperty_singlePropertySaved() {
-        // adjuk meg, mi történjen, ha a repository save-je van meghívva
-        when(propertyRepository.save(any())).thenReturn(propertyForm1);
-
-        // jöhet a tényleges teszt
-        PropertyInfo propertySaved = propertyService.createProperty(propertyForm1);
-        assertEquals(propertyInfo1, propertySaved);
-
-        // ellenőrizzük le, hogy a mockolt erőforrások pont annyiszor voltak meghívva, ahányszor akartuk
-        verify(propertyRepository, times(1)).save(any());
+    void testList_AllProperties1() {
+        when(modelMapper.map(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(propertyInfo1);
+        when(propertyRepository.findAll()).thenReturn(List.of(property1));
+        assertEquals( List.of(propertyInfo1), propertyService.getProperties());
     }
+
+    @Test
+    void testList_AllProperties2() {
+        when(propertyRepository.findAll()).thenReturn(List.of(property1, property2));
+        when(modelMapper.map(property1, PropertyInfo.class)).thenReturn(propertyInfo1);
+        when(modelMapper.map(property2, PropertyInfo.class)).thenReturn(propertyInfo2);
+        assertEquals(List.of(propertyInfo1, propertyInfo2), propertyService.getProperties());
+    }
+
+
+
+
+//    @Test
+//    void testSave_singleProperty_singlePropertySaved() {
+//        when(modelMapper.map(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(propertyInfo1);
+//        when(modelMapper.map(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(property1);
+//        when(propertyRepository.save(any())).thenReturn(property1);
+//
+//        assertEquals(propertyInfo1, propertyService.createProperty(propertyForm1));
+//
+//        verify(propertyRepository, times(1)).save(any());
+//    }
 
 //    @Test
 //    void testList_singlePropertySaved_singlePropertyInList() {
@@ -86,19 +115,7 @@ public class PropertyServiceTest {
 //        verifyNoMoreInteractions(propertyRepository);
 //    }
 
-//    @Test
-//    void testFindByActorRated_multipleMovies_moviesOfActorRated() {
-//        // határozzuk meg, hogy a mock repository mit adjon vissza
-//        when(movieRepository.findByActor("Rupert Grint"))
-//                .thenReturn(List.of(harryPotter4, harryPotter1));
-//
-//        assertThat(movieService.findByActorRated("Rupert Grint"))
-//                .hasSize(2)
-//                .containsExactly(harryPotter1, harryPotter4);
-//
-//        verify(movieRepository, times(1)).findByActor("Rupert Grint");
-//        verifyNoMoreInteractions(movieRepository);
-//    }
+
 
 
 }
