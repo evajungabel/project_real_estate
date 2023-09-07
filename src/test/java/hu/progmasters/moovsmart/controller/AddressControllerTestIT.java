c4bf51c5ba803e6d04923a23e80d1fdaebd1bd40
Kucsera <csaba.kucsera93@gmail.com>
2023-09-07 21:40:13 +0200

    IT tesztek Address.

--- src/test/java/hu/progmasters/moovsmart/controller/AddressControllerTestIT.java
+++ src/test/java/hu/progmasters/moovsmart/controller/AddressControllerTestIT.java
@@ -10,9 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -63,10 +64,35 @@ public class AddressControllerTestIT {
     }
 
     @Test
-    void findByValue() {
+    void findByValue() throws Exception {
+        String value = "mon";
+
+        mockMvc.perform(get("/api/addresses")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(value))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].city", is("Monor")));
     }
 
     @Test
-    void findAllAddress() {
+    void findAllAddress() throws Exception {
+        mockMvc.perform(get("/api/addresses")
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].city", is("Monor")))
+                .andExpect(jsonPath("$[1].city", is("Bénye")))
+                .andExpect(jsonPath("$[2].city", is("Káva")))
+                .andExpect(jsonPath("$[3].city", is("Tök")))
+                .andExpect(jsonPath("$[4].city", is("Zsámbék")))
+                .andExpect(jsonPath("$[5].city", is("Alma")))
+                .andExpect(jsonPath("$[6].city", is("Novigrád")))
+                .andExpect(jsonPath("$[7].city", is("Velen")))
+                .andExpect(jsonPath("$[8].city", is("Tatuin")))
+                .andExpect(jsonPath("$[9].city", is("Pánd")))
+                .andExpect(jsonPath("$[10].city", is("Pilis")))
+                .andExpect(jsonPath("$[11].city", is("Úri")))
+                .andExpect(jsonPath("$[12].city", is("Felcsút")))
+                .andExpect(jsonPath("$[13].city", is("Alcsút-Dobozos")))
+                .andExpect(jsonPath("$[14].city", is("Péteri")));
     }
 }
