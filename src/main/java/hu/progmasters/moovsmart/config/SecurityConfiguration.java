95bbee24b852985b2c22c3c016111a459043bd83
Kucsera <csaba.kucsera93@gmail.com>
2023-09-22 19:27:26 +0200

    logolás alapok

--- src/main/java/hu/progmasters/moovsmart/config/SecurityConfiguration.java
+++ src/main/java/hu/progmasters/moovsmart/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and().authorizeRequests()
                 .antMatchers("/api/**").permitAll()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and().logout()
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(true)
