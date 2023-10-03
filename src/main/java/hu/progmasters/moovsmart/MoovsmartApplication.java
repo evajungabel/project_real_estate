9e245be154053274858a94976001a12b8d4048af
evajungabel <eva.jungabel@gmail.com>
2023-10-03 20:46:41 +0200

    munka

--- src/main/java/hu/progmasters/moovsmart/MoovsmartApplication.java
+++ src/main/java/hu/progmasters/moovsmart/MoovsmartApplication.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @SpringBootApplication
 public class MoovsmartApplication {
     public static void main(String[] args) {
